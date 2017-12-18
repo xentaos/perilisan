8949f72befc92f72c5b59d4809bb4fed3c7cc4ca
Dindin Hernawan <root@dev.xentaos.org>
2017-12-18 16:07:42 +0700

    Create dpkg32.sh

---
+++ fix/dpkg32.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+cd /tmp
+sudo wget -c https://launchpad.net/ubuntu/+archive/primary/+files/dpkg_1.18.4ubuntu1.1_i386.deb    
+sudo tar x dpkg*.deb data.tar.gz    
+sudo tar xfvz data.tar.gz ./usr/bin/dpkg    
+sudo cp ./usr/bin/dpkg /usr/bin/    
+sudo apt-get update    
+sudo apt-get install --reinstall dpkg
